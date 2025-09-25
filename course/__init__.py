52f7ee3dc0ca587fb3b5fed5dfe1ab7110c87c0f
sdiazlor <sdiazlor@gmail.com>
2025-09-25 11:50:46 +0200

    fix: update init

--- course/__init__.py
+++ course/__init__.py
@@ -13,8 +13,8 @@ from course.models import (
     clear_cache,
 )
 from course.slides import show_slides
-from course.plots import create_single_plot, create_multiple_plots, create_pareto_front, create_comparison_plots
-from course.evaluate import evaluate, evaluate_configs
+from course.plots import create_single_plot, create_pareto_front
+from course.evaluate import evaluate_model, evaluate_configs
 
 __all__ = [
     "SMALL_MODEL_IDS",
@@ -23,10 +23,8 @@ __all__ = [
     "ALL_MODEL_IDS",
     "show_slides",
     "create_single_plot",
-    "create_multiple_plots",
     "create_pareto_front",
-    "evaluate",
+    "evaluate_model",
     "evaluate_configs",
-    "create_comparison_plots",
     "clear_cache",
 ]
